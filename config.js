2c7b4d5a8f4051e17db85a3123e3deb19aa84614
killeraliens <killeraliens@outlook.com>
2019-07-11 09:26:17 -0700

    basic form with betterdoc params format

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 const config =  {
-    gmaps: 'AIzaSyBdWN8iDoGf-Hfp_MghCZB5ObUM62Jffxw'
+    gmaps: 'AIzaSyBdWN8iDoGf-Hfp_MghCZB5ObUM62Jffxw',
+    betterDoc: '06a40d3f731a9991ee9828e17aa93395'
 }
--- main.js
+++ main.js
@@ -1,24 +1,25 @@
 //global default search params for better docter api
 const searchParams = {
-    latLong: "33.448376, -112.074036",
-    radius: "10",
+    lat: 33.448376,
+    lng: -112.074036,
+    radius: 10,
     term: "heart"
 }
 
 
 function returnQueryString(paramsObj) {
     return Object.keys(paramsObj).map(key => {
-        return `${encodeURIComponent(key)}=${encodeURIComponent(paramsObj[key])}`;
+        return `${encodeURIComponent(key)}=${paramsObj[key]}`;
     }).join('&');
 }
 
 
 function getGeocode() {
-    $('#step-one-btn').on('click', (e) => {
-
-        if ($('#location').val().length === 0) {
-            renderModal(returnMessageString('Location cannot be blank'));
-        } else {
+    // $('#step-one-btn').on('click', (e) => {
+    //
+    //     if ($('#location').val().length === 0) {
+    //         renderModal(returnMessageString('Location cannot be blank'));
+    //     } else {
 
             const geoCodeParams = {
                 address: $('#location').val(),
@@ -37,15 +38,50 @@ function getGeocode() {
                     throw new Error(response.statusText);
                 })
                 .then( responseJson => {
-                    // console.log(responseJson.results[0].geometry.location);
+                    //console.log(responseJson.results[0].geometry.location);
                     const lat = responseJson.results[0].geometry.location.lat;
                     const lng = responseJson.results[0].geometry.location.lng;
-                    searchParams.latLong = lat + ',' + lng;
-                    console.log(searchParams.latLong);
+                    searchParams.lat = lat;
+                    searchParams.lng = lng;
+                    // console.log(searchParams.latLong);
                 })
-        }
-    })
+                .catch(err => {
+                    // console.log(err.message);
+                    renderModal(returnMessageString(err.message));
+                });
+    //     }
+    // })
 }
+
+
+
+function getBetterDoctor() {
+    $('#search-form').on('submit', (e) => {
+        e.preventDefault();
+
+        getGeocode();
+        console.log($('#search-radius').val());
+
+        const betterDoctorParams = {
+            query: $('#search-term').val(),
+            location:   searchParams.lat + ',' + searchParams.lng + ',' + $('#search-radius').val(),
+            user_location: searchParams.lat + ',' + searchParams.lng,
+            sort: 'distance-asc',
+            skip: 0,
+            limit: 100,
+            user_key: config.betterDoc
+        }
+
+        const rootUrl = 'https://api.betterdoctor.com/2016-03-01/doctors?';
+        const url = rootUrl + returnQueryString(betterDoctorParams);
+
+        console.log(url);
+
+    });
+}
+
+
+
 
 function renderModal(content) {
   $('#modal').css('display', 'block');
@@ -55,15 +91,16 @@ function renderModal(content) {
   handleModalClose();
 }
 
+function handleModalClose() {
+    $('#modal').on('click', '#modal-close', (e) => {
+        $('#modal').css('display', 'none');
+    });
+}
+
 function returnMessageString(message) {
     return `<h3 class="modal-message">${message}</h3>`;
 }
 
-function handleModalClose() {
-  $('#modal').on('click', '#modal-close', (e) => {
-      $('#modal').css('display', 'none');
-  });
-}
 
-
-getGeocode();
+// getGeocode();
+getBetterDoctor();
