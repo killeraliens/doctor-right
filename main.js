859807193414ffe6ab2eb9d1098cda9c6b287075
killeraliens <killeraliens@outlook.com>
2019-07-11 14:52:04 -0700

    ready to build render components

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-//global default search params for better docter api
-const searchParams = {
+//Fetch Param Helpers
+const searchParams = { //global default search params for better doctor api
     lat: 33.448376,
     lng: -112.074036,
     radius: 10,
     term: "heart"
 }
-
 
 function returnQueryString(paramsObj) {
     return Object.keys(paramsObj).map(key => {
@@ -14,9 +13,10 @@ function returnQueryString(paramsObj) {
 }
 
 
+// Fetch Calls
 function getGeocode() {
     // $('#step-one-btn').on('click', (e) => {
-    //
+
     //     if ($('#location').val().length === 0) {
     //         renderModal(returnMessageString('Location cannot be blank'));
     //     } else {
@@ -54,17 +54,19 @@ function getGeocode() {
 }
 
 
-
-function getBetterDoctor() {
-    $('#search-form').on('submit', (e) => {
+function getBetterDoctor(form) {
+    $(`${form}`).on('submit', (e) => {
         e.preventDefault();
 
         getGeocode();
-        console.log($('#search-radius').val());
+
+        const r = document.getElementById("search-radius");
+        const radius = r.options[r.selectedIndex].value;
+        //console.log(radius);
 
         const betterDoctorParams = {
             query: $('#search-term').val(),
-            location:   searchParams.lat + ',' + searchParams.lng + ',' + $('#search-radius').val(),
+            location:   searchParams.lat + ',' + searchParams.lng + ',' + radius,
             user_location: searchParams.lat + ',' + searchParams.lng,
             sort: 'distance-asc',
             skip: 0,
@@ -74,15 +76,57 @@ function getBetterDoctor() {
 
         const rootUrl = 'https://api.betterdoctor.com/2016-03-01/doctors?';
         const url = rootUrl + returnQueryString(betterDoctorParams);
-
         console.log(url);
+
+        fetch(url)
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          }
+          throw new Error(response.statusText);
+        })
+        .then(responseJson => {
+          $(`${form}`).addClass('hidden');
+          renderResultsPage(betterDoctorParams, responseJson);
+          // renderHeader(betterDoctorParams);
+          // renderListings(responseJson);
+        })
+        .catch(err => {
+          renderModal(returnMessageString(err.message));
+        })
 
     });
 }
 
+// Main Components
+function renderResultsPage(params, response) {
+  renderEditNav(params);
+  renderListings(response);
+}
+
+function renderEditNav(betterDoctorParams) {
+   // console.log(betterDoctorParams);
+   const editParams = returnParamsNavString(betterDoctorParams);
+   $('#edit-results-nav').html(editParams);
+   $('#edit-results-nav').removeClass('hidden');
+}
+
+function renderListings(responseJson) {
+  // console.log(responseJson);
+  console.log('do stuff with response');
+}
+
+//Main Html helpers
+function returnParamsNavString(params) {
+  console.log('do stuff with params');
+  console.log(params);
+  // return params.map(param => {
+  //    return `<button class='edit-btn'>${param}</button>`;
+  // }).join('\n');
+}
 
 
-
+// Aside Modal Components
 function renderModal(content) {
   $('#modal').css('display', 'block');
   $('#modal-content').html(
@@ -103,4 +147,4 @@ function returnMessageString(message) {
 
 
 // getGeocode();
-getBetterDoctor();
+getBetterDoctor('#start-form');
