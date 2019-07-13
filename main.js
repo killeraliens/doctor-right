a513f4e7f9cbed7c72d743bd9a0d547d8651650a
killeraliens <killeraliens@outlook.com>
2019-07-13 08:35:44 -0700

    clarifying dev verbiage

--- main.js
+++ main.js
@@ -1,161 +1,5 @@
-// //Fetch Param Helpers
-// const searchParams = { //global default search params for better doctor api
-//     lat: 33.448376,
-//     lng: -112.074036,
-//     radius: 10,
-//     term: "heart"
-// };
 
-// function returnQueryString(paramsObj) {
-//     return Object.keys(paramsObj).map(key => {
-//         return `${encodeURIComponent(key)}=${paramsObj[key]}`;
-//     }).join('&');
-// }
-
-
-// // Fetch Calls
-// function getGeocode(address) {
-//     // $('#step-one-btn').on('click', (e) => {
-
-//     //     if ($('#location').val().length === 0) {
-//     //         renderModal(returnMessageString('Location cannot be blank'));
-//     //     } else {
-
-
-//             const geoCodeParams = {
-//                 address: address,
-//                 key: config.gmaps
-//             }
-
-//             const urlRoot = 'https://maps.googleapis.com/maps/api/geocode/json?';
-//             const url = urlRoot + returnQueryString(geoCodeParams);
-//             console.log(url);
-
-
-//             fetch(url)
-//                 .then(response => {
-//                     if (response.ok) {
-//                         return response.json();
-//                     }
-//                     throw new Error(response.statusText);
-//                 })
-//                 .then( responseJson => {
-
-//                     //console.log(responseJson.results[0].geometry.location);
-//                     const lat = responseJson.results[0].geometry.location.lat;
-//                     const lng = responseJson.results[0].geometry.location.lng;
-//                     searchParams.lat = lat;
-//                     searchParams.lng = lng;
-//                     console.log('AT GEOCODE >>>' + searchParams.lat + searchParams.lng );
-
-//                 })
-//                 .catch(err => {
-//                     renderModal(returnMessageString(err.message));
-//                 });
-//     //     }
-//     // })
-// }
-
-
-// function getBetterDoctor(form) {
-//     $(`${form}`).on('submit', async (e) => {
-//         e.preventDefault();
-
-//         //set global lat + lng
-//         const address = $('#location').val();
-//         await getGeocode(address);
-//         //set global radius
-//         const r = document.getElementById("search-radius");
-//         const radius = r.options[r.selectedIndex].value;
-//         searchParams.radius = radius;
-//         //set global term
-//         searchParams.term = $('#search-term').val();
-
-//         const betterDoctorParams = {
-//             query: searchParams.term,
-//             location:   searchParams.lat + ',' + searchParams.lng + ',' + searchParams.radius,
-//             user_location: searchParams.lat + ',' + searchParams.lng,
-//             sort: 'distance-asc',
-//             skip: 0,
-//             limit: 100,
-//             user_key: config.betterDoc
-//         };
-
-//         const rootUrl = 'https://api.betterdoctor.com/2016-03-01/doctors?';
-//         const url = rootUrl + returnQueryString(betterDoctorParams);
-//         console.log(url);
-
-//         fetch(url)
-//         .then(response => {
-//           if (response.ok) {
-//             return response.json();
-//           }
-//           throw new Error(response.statusText);
-//         })
-//         .then(responseJson => {
-//           $(`${form}`).addClass('hidden');
-//           renderResultsPage(betterDoctorParams, responseJson);
-//           console.log('AT BETTER DOC >>>' + searchParams.term + searchParams.lat + searchParams.lng + searchParams.radius);
-//         })
-//         .catch(err => {
-//           renderModal(returnMessageString(err.message));
-//         })
-
-//     });
-// }
-
-// //returns city and state string for location edit nav button
-// function getReverseGeocode(latLngStr) {
-//     const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latLngStr}&key=${config.gmaps}`;
-
-//     fetch(url)
-//     .then(response => {
-//       if (response.ok) {
-//         return response.json();
-//       }
-//       throw new Error(response.statusText);
-//     })
-//     .then(responseJson => {
-//       const city = responseJson.results[4].address_components[1].long_name;
-//       const stateAbb = responseJson.results[4].address_components[3].short_name;
-//       console.log(city + ', ' + stateAbb);
-//       console.log('AT REVERSE GEOCODE >>>' + searchParams.term + searchParams.lat + searchParams.lng + searchParams.radius);
-//     })
-// }
-
-// // Main Components
-// function renderResultsPage(params, response) {
-//   renderEditNav(params);
-//   renderListings(response);
-// }
-
-// async function renderEditNav(betterDoctorParams) {
-//    // console.log(betterDoctorParams);
-//    const editParams = await returnParamsNavString(betterDoctorParams);
-//    $('#edit-results-nav').html(editParams);
-//    $('#edit-results-nav').removeClass('hidden');
-// }
-
-// function renderListings(responseJson) {
-//   // console.log(responseJson);
-//   console.log('do stuff with response');
-// }
-
-// //Main Html helpers
-// function returnParamsNavString(params) {
-//   console.log('do stuff with params');
-//   console.log(params);
-//   // const latLngArr = params.user_location.split(',');
-//   const location = getReverseGeocode(params.user_location);
-//   // return params.map(param => {
-//   //    return `<button class='edit-btn'>${param}</button>`;
-//   // }).join('\n');
-// }
-
-
-
-
-//Fetch Helpers
+//Global Results State
 const paramsObj = { //global default 'state' of search params referenced in all API calls / param updates
     lat: 33.448376,
     lng: -112.074036,
@@ -163,6 +7,7 @@ const paramsObj = { //global default 'state' of search params referenced in all 
     term: "heart"
 };
 
+//Helpers for Fetching APIs
 function returnQueryString(params) {
     return Object.keys(params).map(key => {
         return `${encodeURIComponent(key)}=${params[key]}`;
@@ -171,7 +16,7 @@ function returnQueryString(params) {
 
 
 // API Calls (utilize/update the global paramsObj)
-function getGeocode(address) {
+function getAndSetParamsGeocode(address) {
 
     const geoCodeParams = {
         address: encodeURIComponent(address),
@@ -202,9 +47,11 @@ function handleFormSubmit(form) {
         e.preventDefault();
 
         const address = $('#location').val();
-        await getGeocode(address)
-        const r = document.getElementById("search-radius");
-        const radius = r.options[r.selectedIndex].value;
+        await getAndSetParamsGeocode(address);
+
+        const selectEl = document.getElementById("search-radius");
+
+        const radius = selectEl.options[selectEl.selectedIndex].value;
         paramsObj.radius = radius;
 
         paramsObj.term = $('#search-term').val();
