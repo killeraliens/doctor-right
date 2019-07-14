b86d6065fc1d534402d61a24474ca43f2175a610
killeraliens <killeraliens@outlook.com>
2019-07-14 12:23:33 -0700

    location edit form updates

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ const paramsObj = { //global default 'state' of search params referenced in all 
     lat: 33.448376,
     lng: -112.074036,
     radius: 10,
-    term: "heart"
+    term: "heart",
+    formattedLocation: ""
 };
 
 //Helpers for Fetching APIs
@@ -42,8 +43,8 @@ function getAndSetParamsGeocode(address) {
       });
 }
 
-function handleFormSubmit(form) {
-    $(`${form}`).on('submit', async (e) => {
+function handleStartFormSubmit() {
+    $('#start-form').on('submit', async (e) => {
         e.preventDefault();
 
         const address = $('#location').val();
@@ -56,7 +57,7 @@ function handleFormSubmit(form) {
 
         paramsObj.term = $('#search-term').val();
 
-        getBetterDoctor(form);
+        getBetterDoctor('#start-form');
     });
 }
 
@@ -68,7 +69,7 @@ function getBetterDoctor(form) {
             user_location: paramsObj.lat + ',' + paramsObj.lng,
             sort: 'distance-asc',
             skip: 0,
-            limit: 10,
+            limit: 30,
             user_key: config.betterDoc
         };
 
@@ -93,17 +94,19 @@ function getBetterDoctor(form) {
 }
 
 
-function renderResultsPage(responseJson) {
+async function renderResultsPage(responseJson) {
   console.log('global params at render>>>');
   console.log(paramsObj);
   // console.log('bd params at render>>>');
   // console.log(betterDoctorParams);
   console.log(responseJson);
 
-  renderNavParams();
+  await renderNavParams();
   handleEditLocationButton();
 
-  $('#listings-title').text(`Medical Professionals Found Near You (${responseJson.data.length})`);
+  $('#listings-title').text(
+    `"${paramsObj.term}" Doctors Found Near ${paramsObj.formattedLocation} (${responseJson.data.length})`
+      );
   renderListDoctors(responseJson);
 }
 
@@ -112,6 +115,7 @@ function renderResultsPage(responseJson) {
 // Nav Components
 async function renderNavParams() {
   let formattedLocation = await getReverseGeocode(paramsObj.lat+','+paramsObj.lng);
+  paramsObj.formattedLocation = formattedLocation;
   $('#nav-params').html(returnParamsNavString(formattedLocation));
   $('#nav-params').css('display', 'block');
 }
@@ -124,14 +128,6 @@ function returnParamsNavString(formattedLocation) {
         <button id="edit-search-term-btn" class="params-btn"><i class="fas fa-search"></i>${paramsObj.term}</button>
       </div>
     `;
-}
-
-// Nav Actions
-function handleEditLocationButton() {
-  $('#nav-params').on('click', '#edit-location-btn', function(e) {
-    e.preventDefault();
-    $('#edit-location-form').slideToggle();
-  })
 }
 
 //Helper API Call returns city and state string for location edit nav button
@@ -156,6 +152,31 @@ function getReverseGeocode(latLngStr) {
     });
 }
 
+// Nav Actions
+function handleEditLocationButton() {
+  $('#nav-params').on('click', '#edit-location-btn', function(e) {
+    e.preventDefault();
+    console.log()
+      $(this).css('border', 'none');
+      $(this).css('background-color', 'Transparent');
+      $('#edit-location-form').slideDown(100, 'linear');
+      handleEditLocationForm();
+  })
+}
+
+function handleEditLocationForm() {
+  $('#edit-location-form').on('submit', async (e) => {
+    e.preventDefault();
+    if ($('#edit-location-input').val() !== '') {
+      const address = $('#edit-location-input').val();
+      await getAndSetParamsGeocode(address);
+      getBetterDoctor('#edit-location-form');
+    } else {
+      renderModal(returnMessageString('You must enter a location'));
+    }
+  })
+}
+
 // Result/Listing Components
 function renderListDoctors(responseJson) {
   $('#list-doctors').html(returnListingsString(responseJson));
@@ -171,11 +192,11 @@ function returnListingsString(responseJson) {
       <li class="doctor-card">
         <img class="avatar" src="${doctor.profile.image_url}" alt="${doctor.profile.slug}"></img>
         <h3>${fullNameTitle}</h3>
-        <p>${returnSpecialtiesString(specialtiesArr)}</p>
+        <p>${returnSpecialtiesDescriptionString(specialtiesArr)}</p>
         <h5>Total practices for this professional (${practices.length})</h5>
         <h5>Locations within your search radius (${whereLocationTrue(practices).length})</h5>
         ${returnLocationsString(whereLocationTrue(practices))}
-        <span>${doctor.specialties[0].name}</span>
+        <span>${returnSpecialtiesNameString(specialtiesArr)}</span>
       </li>
     `;
   }).join('\n');
@@ -224,8 +245,12 @@ function formatPhones(phonesArr) {
   }).join('\n');
 }
 
-function returnSpecialtiesString(specialtiesArr) {
+function returnSpecialtiesDescriptionString(specialtiesArr) {
   return specialtiesArr.map(specialty => `${specialty.description}`).join(' ');
+}
+
+function returnSpecialtiesNameString(specialtiesArr) {
+  return specialtiesArr.map(specialty => `${specialty.name}`).join(', ');
 }
 
 // Aside Modal Components
@@ -247,4 +272,4 @@ function returnMessageString(message) {
     return `<h3 class="modal-message">${message}</h3>`;
 }
 
-handleFormSubmit('#start-form');
+handleStartFormSubmit();
