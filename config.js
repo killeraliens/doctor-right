950b2ce4d012a48142b6124e892f2896cccbd589
killeraliens <killeraliens@outlook.com>
2020-03-17 15:30:40 -0700

    new codes and restrictions

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config =  {
-    gmaps: 'AIzaSyB2ihkos6RnjnqciHUfZf75C6p-UjmrAUM',
+    gmaps: 'AIzaSyBVhNqkrbHB2H6PJ4ylHGSjs0YZq0a5wnc',
     geocode: 'AIzaSyAyFGy_K8BOL2j3qHZNl4QUljybgIhVF1Y',
     betterDoc: '06a40d3f731a9991ee9828e17aa93395'
 }
