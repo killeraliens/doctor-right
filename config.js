8ae628385b62975cad8ea0ba93346ceac148da3b
killeraliens <killeraliens@outlook.com>
2020-03-22 10:25:03 -0700

    new bd user

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config =  {
     gmaps: 'AIzaSyBVhNqkrbHB2H6PJ4ylHGSjs0YZq0a5wnc',
     geocode: 'AIzaSyAyFGy_K8BOL2j3qHZNl4QUljybgIhVF1Y',
-    betterDoc: '06a40d3f731a9991ee9828e17aa93395'
+    betterDoc: 'd3812b9081e98c95fc2fe6091d73ee53'
 }
 
